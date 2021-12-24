291b453f513a6faae57ea9f83b2b4021c09dc777
Felipe Joazeiro <felipejoazeiro@gmail.com>
2021-12-24 11:45:44 -0300

    Start of really config

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,11 @@ import Link from "next/link";
 import React, {Component} from "react";
 
 export default class Index extends Component {
-    static async getInitialProps(){
-        console.log('loaded')
-        return {obj: true}
-    }
-
-
-
     render(){
         return(
             <div>
                 <h2>Iniciado o FRONT</h2>
-                <a href="/">
-                    Link padrão
-                </a>
-                <Link href="/"><span>Link Via next</span></Link>
+                
             </div>
 
         )
