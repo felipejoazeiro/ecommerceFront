781c24e7132e4cc4b5883b0c784c5bbc033bf2c4
Felipe Joazeiro <felipejoazeiro@gmail.com>
2021-12-25 22:38:25 -0300

    carreção de bug

--- pages/carrinho.js
+++ pages/carrinho.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-export default class Index extends Component {
+export default class Carrinho extends Component {
     render(){
         return(
             <div>
--- pages/sucesso.js
+++ pages/sucesso.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-export default class Index extends Component {
+export default class Sucesso extends Component {
     render(){
         return(
             <div>
